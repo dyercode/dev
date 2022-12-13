fc675467ff81f82410b98f98b5cc5b405873eba6
Jon D <dyercode@users.noreply.github.com>
2022-12-13 17:05:02 -0500

    bump versions.
    fmt flake

--- flake.nix
+++ flake.nix
@@ -9,27 +9,26 @@
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
-    let
-      overlays = [
-        (import rust-overlay)
-        (self: super: {
-          rustToolchain = super.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        })
-      ];
+      let
+        overlays = [
+          (import rust-overlay)
+          (self: super: {
+            rustToolchain =
+              super.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          })
+        ];
 
-      rustVersion = pkgs.rust-bin.stable.latest.default;
-      rustPlatform = pkgs.makeRustPlatform {
-        cargo = rustVersion;
-        rustc = rustVersion;
-      };
-      pkgs = import nixpkgs {inherit system overlays; };
-    in
-    {
-      defaultPackage = 
-        rustPlatform.buildRustPackage rec {
-            pname = "dev";
-            version = "ca5efee523eb71e1183e676f186ecb5b62b1bf48";
-            inherit system;
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+        pkgs = import nixpkgs { inherit system overlays; };
+      in {
+        defaultPackage = rustPlatform.buildRustPackage rec {
+          pname = "dev";
+          version = "ca5efee523eb71e1183e676f186ecb5b62b1bf48";
+          inherit system;
 
           src = self;
           cargoSha256 = "sha256-oymy+YZ1/0alJNsy+yVwd1lgoHY8vKaxdWqhD4MCWbA=";
@@ -40,18 +39,15 @@
             license = pkgs.lib.licenses.gpl3;
             maintainers = [ ];
           };
-      };
+        };
 
-      devShells.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          rustToolchain
-          sccache
-        ];
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustToolchain sccache ];
 
-        shellHook = ''
-          export RUSTC_WRAPPER=sccache
-          export RUST_LOG=info
-        '';
-    };
-  });
+          shellHook = ''
+            export RUSTC_WRAPPER=sccache
+            export RUST_LOG=info
+          '';
+        };
+      });
 }
