391bf371a71c7ccd7935c08d80ce33faac350119
Jon D <dyercode@users.noreply.github.com>
2024-04-18 02:59:23 -0400

    fmt.

--- flake.nix
+++ flake.nix
@@ -23,8 +23,18 @@
     };
   };
 
-  outputs = { self, nixpkgs, crane, fenix, flake-utils, advisory-db, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      fenix,
+      flake-utils,
+      advisory-db,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
 
@@ -32,8 +42,7 @@
 
         craneLib = crane.lib.${system};
         ymlFilter = path: _type: builtins.match ".*yml$" path != null;
-        ymlOrCargo = path: type:
-          (ymlFilter path type) || (craneLib.filterCargoSources path type);
+        ymlOrCargo = path: type: (ymlFilter path type) || (craneLib.filterCargoSources path type);
         src = craneLib.cleanCargoSource (craneLib.path ./.);
 
         # Common arguments can be set here to avoid repeating them later
@@ -44,23 +53,26 @@
             filter = ymlOrCargo;
           };
 
-          buildInputs = [
-            # Add additional build inputs here
-          ] ++ lib.optionals pkgs.stdenv.isDarwin [
-            # Additional darwin specific inputs can be set here
-            pkgs.libiconv
-          ];
+          buildInputs =
+            [
+              # Add additional build inputs here
+            ]
+            ++ lib.optionals pkgs.stdenv.isDarwin [
+              # Additional darwin specific inputs can be set here
+              pkgs.libiconv
+            ];
 
           # Additional environment variables can be set directly
           # MY_CUSTOM_VAR = "some value";
         };
 
-        craneLibLLvmTools = craneLib.overrideToolchain
-          (fenix.packages.${system}.complete.withComponents [
+        craneLibLLvmTools = craneLib.overrideToolchain (
+          fenix.packages.${system}.complete.withComponents [
             "cargo"
             "llvm-tools"
             "rustc"
-          ]);
+          ]
+        );
 
         # Build *just* the cargo dependencies, so we can reuse
         # all of that work (e.g. via cachix) when running in CI
@@ -68,52 +80,57 @@
 
         # Build the actual crate itself, reusing the dependency
         # artifacts from above.
-        my-crate =
-          craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
-      in {
-        checks = {
-          # Build the crate as part of `nix flake check` for convenience
-          inherit my-crate;
+        my-crate = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+      in
+      {
+        checks =
+          {
+            # Build the crate as part of `nix flake check` for convenience
+            inherit my-crate;
 
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, resuing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          my-crate-clippy = craneLib.cargoClippy (commonArgs // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-          });
+            # Run clippy (and deny all warnings) on the crate source,
+            # again, resuing the dependency artifacts from above.
+            #
+            # Note that this is done as a separate derivation so that
+            # we can block the CI if there are issues here, but not
+            # prevent downstream consumers from building our crate by itself.
+            my-crate-clippy = craneLib.cargoClippy (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+                cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+              }
+            );
 
-          my-crate-doc =
-            craneLib.cargoDoc (commonArgs // { inherit cargoArtifacts; });
+            my-crate-doc = craneLib.cargoDoc (commonArgs // { inherit cargoArtifacts; });
 
-          # Check formatting
-          my-crate-fmt = craneLib.cargoFmt { inherit src; };
+            # Check formatting
+            my-crate-fmt = craneLib.cargoFmt { inherit src; };
 
-          # Audit dependencies
-          my-crate-audit = craneLib.cargoAudit { inherit src advisory-db; };
+            # Audit dependencies
+            my-crate-audit = craneLib.cargoAudit { inherit src advisory-db; };
 
-          # Run tests with cargo-nextest
-          # Consider setting `doCheck = false` on `my-crate` if you do not want
-          # the tests to run twice
-          my-crate-nextest = craneLib.cargoNextest (commonArgs // {
-            inherit cargoArtifacts;
-            partitions = 1;
-            partitionType = "count";
-          });
-        } // lib.optionalAttrs (system == "x86_64-linux") {
-          # NB: cargo-tarpaulin only supports x86_64 systems
-          # Check code coverage (note: this will not upload coverage anywhere)
-          my-crate-coverage =
-            craneLib.cargoTarpaulin (commonArgs // { inherit cargoArtifacts; });
-        };
+            # Run tests with cargo-nextest
+            # Consider setting `doCheck = false` on `my-crate` if you do not want
+            # the tests to run twice
+            my-crate-nextest = craneLib.cargoNextest (
+              commonArgs
+              // {
+                inherit cargoArtifacts;
+                partitions = 1;
+                partitionType = "count";
+              }
+            );
+          }
+          // lib.optionalAttrs (system == "x86_64-linux") {
+            # NB: cargo-tarpaulin only supports x86_64 systems
+            # Check code coverage (note: this will not upload coverage anywhere)
+            my-crate-coverage = craneLib.cargoTarpaulin (commonArgs // { inherit cargoArtifacts; });
+          };
 
         packages = {
           default = my-crate;
-          my-crate-llvm-coverage = craneLibLLvmTools.cargoLlvmCov
-            (commonArgs // { inherit cargoArtifacts; });
+          my-crate-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // { inherit cargoArtifacts; });
         };
 
         apps.default = flake-utils.lib.mkApp { drv = my-crate; };
@@ -126,13 +143,17 @@
           # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
 
           # Extra inputs can be added here; cargo and rustc are provided by default.
-          packages =
-            [ pkgs.sccache self.packages.${system}.default pkgs.cargo-edit ];
+          packages = [
+            pkgs.sccache
+            self.packages.${system}.default
+            pkgs.cargo-edit
+          ];
 
           shellHook = ''
             export RUSTC_WRAPPER=sccache
             export RUST_LOG=info
           '';
         };
-      });
+      }
+    );
 }
